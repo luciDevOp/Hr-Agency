d23b457448bcd7994dfb6030abc1183d9adb79aa
Luci <lucian.ungureanu@breakingpoint.ro>
2026-05-18 14:23:04 +0300

    modificare candidate controller

--- api/app/Controllers/CandidateController.php
+++ api/app/Controllers/CandidateController.php
@@ -83,13 +83,14 @@ class CandidateController extends BaseController
         }
     
         $db->transComplete(); // Commit transaction
+        $emailResults = [];
         if ($jobId !== null) {
             $email = \Config\Services::email();
             $email->initialize([
                 'protocol' => 'smtp',
                 'SMTPHost' => 'mail.hha.ro',
                 'SMTPUser' => 'jobs@hha.ro',
-                'SMTPPass' => 'jobs.hha',
+                'SMTPPass' => '(=#j9[6c8DD7E{M5',
                 'SMTPPort' => 465,
                 'SMTPCrypto' => 'ssl', // or 'tls' with port 587
                 'mailType' => 'html',
@@ -117,14 +118,52 @@ class CandidateController extends BaseController
                 $email->attach($cvPath);
             }
         
-            if (!$email->send()) {
-                
+            if ($email->send()) {
+                $emailResults['office_email'] = 'sent';
+            } else {
+                $emailResults['office_email'] = 'failed';
+                $emailResults['office_email_error'] = $email->printDebugger(['headers']);
+            }
+
+            // Send confirmation email to the candidate
+            $confirmEmail = \Config\Services::email();
+            $confirmEmail->initialize([
+                'protocol' => 'smtp',
+                'SMTPHost' => 'mail.hha.ro',
+                'SMTPUser' => 'jobs@hha.ro',
+                'SMTPPass' => '(=#j9[6c8DD7E{M5',
+                'SMTPPort' => 465,
+                'SMTPCrypto' => 'ssl',
+                'mailType' => 'html',
+                'charset' => 'utf-8',
+            ]);
+
+            $candidateEmail = $this->request->getPost('email');
+            $confirmEmail->setFrom('jobs@hha.ro', 'Head Hunting Agency');
+            $confirmEmail->setTo($candidateEmail);
+            $confirmEmail->setSubject('Thank you for your application – Head Hunting Agency');
+            $confirmEmail->setMessage("
+                <p>Thank you for submitting your application on our site.</p>
+                <p>We appreciate the time and effort you've invested in sharing your qualifications and experiences with us.</p>
+                <p>We are currently in the process of reviewing all applications.</p>
+                <p>If your profile is shortlisted, we will reach out to you directly to discuss the next steps.</p>
+                <p>Please note that due to the volume of applications, we may not be able to respond to every applicant individually.</p>
+                <p>We encourage you to stay connected with us for future opportunities as well.</p>
+                <p>Have a good day!</p>
+            ");
+
+            if ($confirmEmail->send()) {
+                $emailResults['candidate_email'] = 'sent';
+            } else {
+                $emailResults['candidate_email'] = 'failed';
+                $emailResults['candidate_email_error'] = $confirmEmail->printDebugger(['headers']);
             }
         }
         
 
         return $this->response->setJSON([
             'status' => 'success',
+            'emails' => $emailResults,
         ]);
 
     }
